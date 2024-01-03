69fff98fc56143357753377eef881cf326468bea
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-01-03 11:56:43 -0600

    progress

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,6 @@
-import { eq } from 'drizzle-orm';
-import { dogs, owners } from './schema.mjs';
-import { db } from './db.mjs';
+import { eq } from "drizzle-orm";
+import { dogs, owners } from "./schema.mjs";
+import { db } from "./db.mjs";
 
 // Delete all records from the tables.
 await db.delete(dogs);
@@ -9,37 +9,37 @@ await db.delete(owners);
 // Insert new rows in the owners table.
 let results = await db
   .insert(owners)
-  .values({ name: 'Tami' })
+  .values({ name: "Tami" })
   .returning({ id: owners.id });
 const tamiId = results[0].id;
 
 results = await db
   .insert(owners)
-  .values({ name: 'Amanda' })
+  .values({ name: "Amanda" })
   .returning({ id: owners.id });
 const amandaId = results[0].id;
 
 // Insert new rows in the dogs table.
 await db
   .insert(dogs)
-  .values({ name: 'Comet', breed: 'Greyhound', ownerId: tamiId });
+  .values({ name: "Comet", breed: "Greyhound", ownerId: tamiId });
 await db.insert(dogs).values([
-  { name: 'Maisey', breed: 'Treeing Walker Coonhound', ownerId: amandaId },
-  { name: 'Ramsay', breed: 'Native American Indian Dog' },
-  { name: 'Oscar', breed: 'German Shorthaired Pointer', ownerId: amandaId }
+  { name: "Maisey", breed: "Treeing Walker Coonhound", ownerId: amandaId },
+  { name: "Ramsay", breed: "Native American Indian Dog" },
+  { name: "Oscar", breed: "German Shorthaired Pointer", ownerId: amandaId },
 ]);
 
 // Modify a row in the dogs table.
-await db.update(dogs).set({ breed: 'Whippet' }).where(eq(dogs.name, 'Comet'));
+await db.update(dogs).set({ breed: "Whippet" }).where(eq(dogs.name, "Comet"));
 
 // Delete a row from the dogs table.
-await db.delete(dogs).where(eq(dogs.name, 'Ramsay'));
+await db.delete(dogs).where(eq(dogs.name, "Ramsay"));
 
 // Get all dogs.
 results = await db.select().from(dogs);
-console.log('All Dogs');
+console.log("All Dogs");
 for (const result of results) {
-  console.log('-', result.name);
+  console.log("-", result.name);
 }
 
 // Get dogs owned by Amanda.
@@ -47,10 +47,10 @@ results = await db
   .select()
   .from(dogs)
   .innerJoin(owners, eq(owners.id, dogs.ownerId))
-  .where(eq(owners.name, 'Amanda'));
+  .where(eq(owners.name, "Amanda"));
 console.log("\nAmanda's Dogs");
 for (const result of results) {
-  console.log('-', result.dogs.name);
+  console.log("-", result.dogs.name);
 }
 
 process.exit(); // Why needed?
