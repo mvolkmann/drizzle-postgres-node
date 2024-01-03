adc604d66f874332e176edfc16c894260d0055cf
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-01-03 15:08:59 -0600

    progress

--- src/db.mjs
+++ src/db.mjs
@@ -1,11 +1,10 @@
 import { drizzle } from "drizzle-orm/postgres-js";
-import pg from "pg";
+import postgres from "postgres";
 import * as schema from "./schema.mjs";
 
 const dbName = "drizzle-demo";
 const dbPrefix = "postgres://postgres:adminadmin@0.0.0.0:5432";
 const connectionString = dbPrefix + "/" + dbName;
-const client = new pg.Client({ connectionString });
-await client.connect();
+const client = postgres(connectionString);
 
 export const db = drizzle(client, { schema });
